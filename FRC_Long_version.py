709028ce3a15f9d879425858c84295572ab6c4cd
Sina <sinagolchi@gmail.com>
2022-04-17 18:58:13 -0400

    Implemented many features for the simplified versions (look at change log V4.8.2 for details)

--- FRC_Long_version.py
+++ FRC_Long_version.py
@@ -8,37 +8,49 @@ import seaborn as sns
 
 st.set_page_config(layout='wide',page_title='FRC Game Companion',page_icon='FRC Logo White-100px.png') #set streamlit page to wide mode
 
+game_type = 'simplified'
+
 def refresh():
     st.experimental_rerun()
 
-user_dict = {
-    'M':'Mayor',
-    'LEF':'Large Engineering Firm',
-    'DP': 'District Planner',
-    'EM': 'Emergency Manager',
-    'ENGO': 'Environmental ENGO',
-    'F': 'Farmer',
-    'FP': 'Federal Government',
-    'FN': 'First Nations',
-    'I': 'Insurance Company',
-    'J': 'Journalist',
-    'LD': 'Land Developer',
-    'LBO': 'Local Business',
-    'PUC': 'Power Utility',
-    'CRA-HV': 'Community Residence - High Value',
-    'CRA-MHA': 'Community Residence - Mobile Home',
-    'CRA-MV': ' Community Residence - Mediume value',
-    'PH': 'Hydrologist',
-    'PP': 'Provincial Politician',
-    'TA': 'Transport Authority',
-    'WW': 'Waste and Water Treatment Director'
-}
+if game_type == 'full':
+    user_dict = {
+        'M':'Mayor',
+        'LEF':'Large Engineering Firm',
+        'DP': 'District Planner',
+        'EM': 'Emergency Manager',
+        'ENGO': 'Environmental ENGO',
+        'F': 'Farmer',
+        'FP': 'Federal Government',
+        'FN': 'First Nations',
+        'I': 'Insurance Company',
+        'J': 'Journalist',
+        'LD': 'Land Developer',
+        'LBO': 'Local Business',
+        'PUC': 'Power Utility',
+        'CRA-HV': 'Community Residence - High Value',
+        'CRA-MHA': 'Community Residence - Mobile Home',
+        'CRA-MV': ' Community Residence - Mediume value',
+        'PH': 'Hydrologist',
+        'PP': 'Provincial Politician',
+        'TA': 'Transport Authority',
+        'WW': 'Waste and Water Treatment Director'
+    }
+
+else:
+    user_dict = {'M': "Mayor", 'P': 'Planner', 'EM': 'Emergency Manager', 'CSO': 'Community Service',
+                 'WR': 'Waterfront Resident', 'F': 'Farmer', 'LD': 'Land Developer', 'LEF': 'Large Engineering Firm'}
 user_dict_inv= {v:k for k,v in user_dict.items()}
 
 phase_dict = {0: 'Adjusting tax rate (for government only)', 1: 'Phase 3: Updating Budget', 2: 'Phase 1A: FRM Measure bidding', 3: 'Phase 1B: Transactions', 4: 'Phase 2: Flood and damage analysis', 5: 'Phase 4: Vote'}
 
-def init_connection():
-    return psycopg2.connect(**st.secrets["postgres"])
+
+if game_type == 'full':
+    def init_connection():
+        return psycopg2.connect(**st.secrets["postgres"])
+else:
+    def init_connection():
+        return psycopg2.connect(options='-c search_path=FRC_s',**st.secrets["postgres"])
 
 conn = init_connection()
 
@@ -46,7 +58,42 @@ conn = init_connection()
 def get_sql(table):
     return pd.read_sql("SELECT * from " + table+";",conn)
 
-
+def choose_role(user:str,board:int):
+    with st.container():
+        st.info('It seems that you do not have a role yet, please choose one below:')
+        st.warning('Note: If the role you chose is taken already you will receive a prompt to choose another')
+        dfr = get_sql('frc_users')
+        dfr.set_index('user',inplace=True)
+        chosen_roles = list(dfr[dfr['board'] == 1]['role'])
+        print(chosen_roles)
+        roles = user_dict.keys()
+        role_options = [user_dict[role] for role in roles if role not in chosen_roles]
+        role_selection = st.radio(label='Choose a role and confirm', options=role_options)
+        def confirm_selection(selection,user_name):
+            dfr = get_sql('frc_users')
+            dfr.set_index('user',inplace=True)
+            dfr = dfr[dfr['board']==board]
+            print(dfr)
+            if selection not in dfr['role']:
+                curB = conn.cursor()
+                try:
+                    curB.execute("UPDATE frc_users SET role=%s WHERE name=%s;",(user_dict_inv[selection],str(dfr.loc[user_name,'name'])))
+                    conn.commit()
+                except:
+                    st.error('This role was taken just now, try again.')
+                    time.sleep(1)
+                    st.experimental_rerun()
+                st.balloons()
+                time.sleep(1)
+                st.experimental_rerun()
+            else:
+                st.error('This role was taken just now, try again.')
+                time.sleep(1)
+                st.experimental_rerun()
+        onclick = st.button(label='Confirm selection')
+        if onclick:
+            confirm_selection(role_selection,user)
+        st.stop()
 # with st.expander('Developer tools'):
 #     st.dataframe(df_m)
 
@@ -55,11 +102,11 @@ st.title('FRC Game companion WebApp')
 st.caption('Developed by Sina Golchi with collaboration with FRC Team under creative commons license')
 
 #sidebar and login system
-with st.sidebar:
-    st.write('Please Login below:')
-    user_name = st.text_input('Your unique FRC ID',type='password')
-    user_roster = get_sql('frc_users')
-    user_roster.set_index('user',inplace=True)
+
+st.sidebar.write('Please Login below:')
+user_name = st.sidebar.text_input('Your unique FRC ID',type='password')
+user_roster = get_sql('frc_users')
+user_roster.set_index('user',inplace=True)
 try:
     st.sidebar.success('Welcome '+ user_roster.loc[user_name,'name'])
 except Exception as e:
@@ -72,6 +119,7 @@ except Exception as e:
         st.error('Your unique ID is incorrect, please contact FRC admins for help!')
         st.stop()
 
+
 with st.sidebar:
     if user_roster.loc[user_name,'level'] > 1:
         board = st.selectbox(label='FRC Board number', options=[1, 2, 3, 4, 5, 6])
@@ -79,6 +127,7 @@ with st.sidebar:
     else:
         board = int(user_roster.loc[user_name,'board'])
         user_id = user_roster.loc[user_name,'role']
+
     df = get_sql('budget_lb' + str(board))
     df.set_index('role', inplace=True)
     df_m = get_sql('measures_lb1')
@@ -94,6 +143,9 @@ with st.sidebar:
     confirm_rerun = st.button(label='Refresh Data')
     if confirm_rerun:
         refresh()
+
+if user_id == None:
+    choose_role(user_name,board)
 
 st.header("Your role is: " + str(user_dict[user_id]) + " on board " + str(board))
 
@@ -478,6 +530,11 @@ def taxes_section():
                 process_m_p(user_id, int(df_v.loc[board, 'power_price']), 'PUC')
                 set_as_paid(user_id)
 
+def tax_section_short():
+    st.markdown("""___""")
+    st.info('Tax payements are processed automatically')
+
+
 def bidding_section():
     st.markdown("""___""")
     def make_bid_func(measure, amount):
@@ -546,6 +603,24 @@ def bidding_section():
                         'r' + str(g_round) + '_bid'].to_list()]) / df_m.loc[measure, 'cost'] * 100))
                 except:
                     st.warning('The bid on this measure have exceeded the cost, please readjust bids')
+
+    df_impl_measures = get_sql('impl_measures'+str(board))
+    st.subheader('Implemented measures')
+    for m_row in df_impl_measures.iterrows():
+        m_row = m_row[1]
+        if m_row['round'] == g_round:
+            col1, col2,col3 = st.columns([1,3,1])
+            with col1:
+                st.metric(label=m_row['measure'],
+                              value=str(sum(m_row['amounts'])) + r"/" + str(
+                                  int(df_m.loc[m_row['measure'], 'cost'])))
+            with col2:
+                st.caption(
+                    'Bidders: ' + ',  '.join([user_dict[p] + ': $' + str(b) for p, b in zip(m_row['biders'], m_row['amounts'])]))
+
+                st.progress(int(sum(m_row['amounts']) / df_m.loc[m_row['measure'], 'cost'] * 100))
+            with col3:
+                st.success('Implemented')
 
     confirm_rerun = st.button(label='Refresh Data',key='bidding section')
     if confirm_rerun:
@@ -629,6 +704,59 @@ def transaction_section():
             df_p_log['Timestamp'] = df_p_log['Timestamp'].dt.tz_convert('EST').dt.strftime('%B %d, %Y, %r')
             st.dataframe(df_p_log)
 
+#Transactions for short version
+def transations_short():
+    def money_transfer(amount, r_party):
+        curA = conn.cursor()
+        curA.execute(update_budget, (int(board), int(df.loc[user_id, 'cb']) - amount, user_id))
+        curA.execute(update_delta, (int(board), -amount, user_id))
+        curA.execute(update_budget, (int(board), int(df.loc[r_party, 'cb'] + amount), r_party))
+        curA.execute(update_delta, (int(board), +amount, r_party))
+        curA.execute(log_transaction, (int(board), user_dict[user_id], amount, user_dict[r_party]))
+        conn.commit()
+
+    st.header("Phase 1B: Transactions")
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        t_amount = int(st.selectbox(label='Budget to transfer', options=[x for x in range(1, 10)]))
+    with col2:
+        party = user_dict_inv[
+            st.selectbox(options=[user_dict[x] for x in other_roles], label='Stakeholder receiving')]
+    with col4:
+        transfer = st.button(label='Complete transaction', help='Only click when you are absolutely sure')
+    with col3:
+        st.metric(label='Budget after transaction', value='$' + str(df.loc[user_id, 'cb'] - t_amount),
+                  delta=-t_amount)
+
+    if transfer:
+        money_transfer(t_amount, party)
+        with st.spinner('Performing transaction'):
+            time.sleep(3)
+        st.success('The transaction to ' + party + ' was successful')
+        time.sleep(3)
+        st.experimental_rerun()
+
+    def styler(val):
+        color = 'green' if val == user_dict[user_id] else None
+        return 'color: %s' % color
+
+    st.subheader('History')
+    st.caption("Only transaction relating to you will show up here")
+    df_p_log = get_sql('payment' + str(board))
+    est = pytz.timezone('EST')
+    df_p_log = df_p_log.rename(
+        columns={'datetime': 'Timestamp', 'from_user': 'Sender', 'amount': 'Transaction total',
+                 'to_user': 'Receiving party'})
+    df_p_log['id'] = [int(p) for p in df_p_log['id']]
+    df_p_log.set_index('id', inplace=True)
+    #df_pesonalized = df_p_log[df_p_log['Sender']==user_dict_inv[user_id] or df_p_log['Receiving party']==user_dict_inv[user_id]]
+    if not df_p_log.empty:
+        df_p_log['Timestamp'] = df_p_log['Timestamp'].dt.tz_convert('EST').dt.strftime('%B %d, %Y, %r')
+        df_pesonalized = df_p_log[(df_p_log['Sender'] == user_dict[user_id]) | (df_p_log['Receiving party'] == user_dict[user_id])]
+        df_pesonalized = df_pesonalized.style.applymap(styler)
+        st.dataframe(df_pesonalized)
+    else:
+        st.info("No transaction to show")
 #Voting section
 
 update_vote_DB = ("UPDATE budget_lb%s SET r%s_vote=ARRAY[%s,%s,%s] WHERE role=%s;")
@@ -684,6 +812,50 @@ def voting():
     else:
         st.info('Awaiting results')
 
+def vote_short():
+    df = get_sql('budget_lb' + str(board))
+    df.set_index('role',inplace=True)
+    st.header('Round ' + str(g_round) + ' vote of confidence')
+    if df.loc[user_id,'r'+str(g_round)+'_vote'] is None:
+        def submit_vote(results, user):
+            curA = conn.cursor()
+            curA.execute("UPDATE budget_lb%s SET r%s_vote=ARRAY[%s] WHERE role=%s;", (int(board),int(g_round), *results, user))
+            conn.commit()
+            with st.spinner('submitting your vote'):
+                time.sleep(1)
+            st.success('Your vote is submitted, awaiting results')
+
+
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            st.markdown('Mayor')
+            M_vote = st.radio(key='M_vote',label='Your vote',options=['In Favor','Against'])
+        with col2:
+            approve_vote = st.button(label='submit your vote')
+
+        if approve_vote:
+            submit_vote([M_vote],user_id)
+
+
+    elif not [i for i in df.loc[:,'r'+str(g_round)+'_vote'] if i == None] or vote_override:
+        vote = []
+        vote_g_round = []
+        official = []
+        for r in range(1,4):
+            for v in df.loc[:,'r'+str(r)+'_vote']:
+                if v is not None:
+                    for i, o in zip(range(1),['Mayor']):
+                        vote.append(v[i])
+                        official.append(o)
+                        vote_g_round.append(r)
+        df_vote_result = pd.DataFrame(zip(vote,official,vote_g_round),columns=['Votes','Official','Game round'])
+        sns.set_theme(style='darkgrid',palette='colorblind')
+        fig = sns.catplot(data=df_vote_result,x='Votes',col='Official',kind='count',row='Game round',size=5,aspect=2)
+        st.pyplot(fig)
+
+    else:
+        st.info('Awaiting results')
+
 
 def flood():
     qulified_for_DRP = ['CRA-HV','CRA-MV','CRA-MHA','ENGO','F']
@@ -711,7 +883,7 @@ def flood():
         st.success('You are not affected by the flood')
 
 
-dict_phase_case = {0:tax_increacse_section ,1:taxes_section, 2: bidding_section, 3:transaction_section, 4:flood, 5:voting}
+dict_phase_case = {0:tax_increacse_section ,1:tax_section_short, 2: bidding_section, 3:transaction_section, 4:flood, 5:vote_short}
 
 if dict_phase_case[df_v.loc[board,'phase']] is not None:
     dict_phase_case[df_v.loc[board,'phase']]()
@@ -728,6 +900,11 @@ if user_id == 'PH':
         with col3:
             st.image('imgs/SLR test.png')
             st.image('imgs/Storm surge winter flooding.png')
+
+if game_type == 'simplified':
+    st.markdown("""___""")
+    with st.expander('Secret transactions'):
+        transations_short()
 
 #function for buying insurance
 insurance_update = ("UPDATE budget_lb%s SET r%s_insurance = %s WHERE role=%s;")
@@ -807,9 +984,10 @@ with st.sidebar:
         else:
             st.success('your property is insured for round ' + str(g_round))
             st.info('You can no longer cancel your insurance for this round')
+
 st.markdown('''---''')
 st.subheader('Miro board ' + str(int(board)))
-miro_dict = {1:['https://miro.com/app/live-embed/uXjVOJ_4_5c=/?moveToViewport=-23351,-9416,27515,14305&embedAutoplay=true','https://miro.com/app/board/uXjVOJ_4_5c=/?invite_link_id=148561260422'],
+miro_dict = {1:['https://miro.com/app/live-embed/o9J_lNDWMVw=/?moveToViewport=-17253,-6353,15383,6977&embedAutoplay=true','https://miro.com/app/board/o9J_lNDWMVw=/?share_link_id=717927830108'],
              2:['https://miro.com/app/live-embed/uXjVOR_gfI0=/?moveToViewport=-23351,-9416,27515,14305&embedAutoplay=true','https://miro.com/app/board/uXjVOR_gfI0=/?invite_link_id=53493355924'],
              3:['https://miro.com/app/live-embed/uXjVOR_g16s=/?moveToViewport=-23351,-9416,27515,14305&embedAutoplay=true','https://miro.com/app/board/uXjVOR_g16s=/?invite_link_id=349135164503'],
              4:['https://miro.com/app/live-embed/uXjVOR_hQ8o=/?moveToViewport=-23351,-9416,27515,14305&embedAutoplay=true','https://miro.com/app/board/uXjVOR_hQ8o=/?invite_link_id=471512594109'],
@@ -817,6 +995,6 @@ miro_dict = {1:['https://miro.com/app/live-embed/uXjVOJ_4_5c=/?moveToViewport=-2
              6:['https://miro.com/app/live-embed/uXjVOR_h1vw=/?moveToViewport=-23351,-9416,27515,14305&embedAutoplay=true','https://miro.com/app/board/uXjVOR_h1vw=/?invite_link_id=87971323805']}
 
 with st.expander('Miro board', expanded=True):
-    components.iframe(miro_dict[int(board)][0],height=740)
+    components.iframe(miro_dict[int(board)][0],height=700,scrolling=False)
 
     st.write("Open board in a new tab [link]("+miro_dict[int(board)][1]+')')
